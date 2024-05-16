5da4d2ebbfbbd26f77a2124001dc965e5f85690c
milind.sansare <milind.sansare@ayekart.com>
2024-05-16 09:25:51 +0530

    initial commit

---
+++ frontend/src/container/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './about/About.jsx';
+export { default as Footer } from './footer/Footer.jsx';
+export { default as Header } from './header/Header.jsx';
+export { default as Skills } from './skills/Skills.jsx';
+export { default as Work } from './work/Work.jsx';
---
+++ sanity/schemaTypes/index.js
@@ -0,0 +1,7 @@
+import testimonials from "./testimonials"
+import abouts from "./abouts"
+import skills from "./skills"
+import experiences from "./experiences"
+import workExperience from "./workExperience"
+import work from "./work"
+export const schemaTypes = [testimonials, abouts, skills, workExperience, experiences, work]
